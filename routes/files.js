bf41af53352427caca75c5a2042b3d9c4a754897
Vishesh Garg <visheshgarg5872@gmail.com>
2023-04-28 17:08:45 +0530

    updated file upload folder to __dirname

--- routes/files.js
+++ routes/files.js
@@ -5,7 +5,7 @@ const File = require('../models/file');
 const { v4: uuidv4 } = require('uuid');
 
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/') ,
+    destination: (req, file, cb) => cb(null, __dirname) ,
     filename: (req, file, cb) => {
         const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
               cb(null, uniqueName)
